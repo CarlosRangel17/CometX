21c2a40ff647c6e3a76a2d7040f341cde3e22ae0
Carlos Rangel <carlos.developer17@gmail.com>
2019-07-17 15:56:16 -0500

    Added Lexicon extension class & nuget package files

---
+++ CometX/.NET Core/CometX.NETCore.Entities/Extensions/LexiconExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using CometX.NETCore.Entities.GenericEntity;
+
+namespace CometX.NETCore.Entities.Extensions
+{
+    public static class LexiconExtension
+    {
+        public static T GetValueByKey<T>(this List<Lexicon> list, string key) where T : new()
+        {
+            var value = list.First(x => x.Key.Equals(key)).Value;
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+
+        public static string GetStringValueByKey(this List<Lexicon> list, string key)
+        {
+            var value = list.First(x => x.Key.Equals(key)).Value;
+            return value.ToString();
+        }
+    }
+}
